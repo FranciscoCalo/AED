3c2468124341573c098568f30b474afd9d752c2c
FranciscoCalo <103156388+FranciscoCalo@users.noreply.github.com>
2022-05-13 20:58:04 -0300

    Update AssertTest.cpp

--- 01cppAssertTest/AssertTest.cpp
+++ 01cppAssertTest/AssertTest.cpp
@@ -1,53 +1,49 @@
 #include <cassert>
-//#include <iostream>
-//#include <iomanip>
 #include <string>
 
 using namespace std::literals;
 
 int main()
 {
-    assert(2 == 1 + 1);
+    // Prueba de tipo de dato int
+
+    assert(1 + 1 == 2);
+    assert(5 - 6 == -1);
+    assert(1 * 2 == 2);
+    assert(4 / 2 == 2);
+    assert(5 % 2 == 1);
+    assert(25 / 5 < 30);
+    assert(26 > 15 and 26 > 20);
+    assert(5 != 4);
+
 
     //Prueba de tipo de dato bool
-    assert(true);
-    assert(false == false);
-    assert(true != false);
-    assert(not false);
-    assert(not false == true);
-    assert(false or true);
-    assert(true and true);
-    assert(false or true and false == false);
-    assert((false or true) and false == false);
-
+    assert(true and (false + 1));
+    assert(false or true and true);
     assert(true + true - 1);
     assert(false + 1);
     assert(false + 5 - 4);
+    
 
     //Prueba de tipo de dato double
-    assert(2.0 == 1.0 + 1.0);
-    assert(0.1 == 1.0 / 10.0);
-    assert(1 == 1.0);
-    assert(2.0 == 1 + 1.0);
-    assert(2 == 1 + 1.0);
-    //assert(1.0 == 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1);
-    //std::cout << std::setprecision(17) << 1.0/5.0 << '\n';
-
+    assert(1.0 + 1.0 == 2);
     assert(1.0 * 1.0 == 1.0);
     assert(1.5 * 2 == 3.0);
+    assert(1.0 - 5 == -4);
+    
 
     //Prueba de strings
-    assert("pacheco"s == "pa"s + "checo"s);
-    assert("pacheco"s.length() == 7); //length("pacheco")
-
-    /* Posibles pruebas :   Quitar caracteres? pacheco - co = pache
-                            Quitar caracteres en lugares especificos? pacheco - che = paco
-                            Cambiar minusculas por mayusculas? (quitar caracteres en lugares especificos + sumar) pacheco + P = Pacheco
-                            */
-
+    assert("prue"s + "ba"s == "prueba"s);
+    assert("prueba"s > "auto"s);
+    assert("prueba"s < "z"s);
+    assert("prueba"s == "prueba"s);
+    assert("prueba"s == "auto"s or "prueba"s != "auto"s);
+    
 
     //Prueba de chars
+    assert('A' > 2);
     assert('A' == 65);
     assert('A' + '!' == 65 + 33);
-    assert("A" == "A"s);
+    assert('A' != '60');
+
 }
